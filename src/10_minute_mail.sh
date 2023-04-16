0102c3a85e4304cd574a0fa81636cf2fc5849d22
zeviel <77536370+zeviel@users.noreply.github.com>
2023-04-16 20:18:03 +0800

    Update 10_minute_mail.sh

--- src/10_minute_mail.sh
+++ src/10_minute_mail.sh
@@ -2,11 +2,12 @@
 
 api="https://ext2.10minemail.com"
 token=null
+user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36"
 
 function generate_email() {
 	response=$(curl --request POST \
 		--url "$api/mailbox" \
-		--user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36" \
+		--user-agent "$user_agent" \
 		--header "content-type: application/json")
 	if [ -n $(jq -r ".token" <<< "$response") ]; then
 		token=$(jq -r ".token" <<< "$response")
@@ -18,7 +19,7 @@ function generate_email() {
 function get_messages() {
 	curl --request POST \
 		--url "$api/messages" \
-		--user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36" \
+		--user-agent "$user_agent" \
 		--header "content-type: application/json" \
 		--header "authorization: Bearer $token"
 }
